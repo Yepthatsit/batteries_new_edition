537ae33b0d4e4af2b4fd1cf640ffd6038ee687c7
Yepthatsit <karolgrzywa8@gmail.com>
2024-07-04 22:20:10 +0200

    Meaters communication setup

--- main_demo_version.py
+++ main_demo_version.py
@@ -3,11 +3,15 @@ import serial
 from time import sleep
 from serial.tools.list_ports import comports
 #################################   SET PARAMETERS HERE ###############################
+stabilization_mode = True 
+
+
 filename = ""
 ramp_rate = 0.2
 start_setpoint = 300
 source_voltage = 2
 end_setpoint = 70
+
 
 #########################################################
 class qSerial(serial.Serial):
@@ -33,15 +37,19 @@ class qSerial(serial.Serial):
         for i in ports:
             print(i) 
 def main() ->None:
-    global filename,start_setpoint,end_setpoint,ramp_rate,source_voltage
+    global filename,start_setpoint,end_setpoint,ramp_rate,source_voltage,stabilization_mode
     Lakeshore  = qSerial.qSerial(port='COM3',baudrate=57600,bytesize=serial.SEVENBITS,stopbits=serial.STOPBITS_ONE,parity=serial.PARITY_ODD,timeout=10)
     rm = pyvisa.ResourceManager()
     kithley = rm.open_resource('USB0::0x05E6::0x2460::04452187::INSTR')
     kithley.write("OUTP ON")
+    kithley.query(':MEAS:CURR?')
+    kithley.write('SOUR:FUNC VOLT')
     kithley.write(f'SOUR:VOLT {source_voltage}')
-    
+    Lakeshore.write(bytes(f"RAMP 1,1,{ramp_rate}\r\n","utf-8"))
     sleep(1)
-    
-    pass
+    if stabilization_mode:
+        pass
+    else:
+        raise NotImplementedError
 if __name__ == "__main__":
     main()
